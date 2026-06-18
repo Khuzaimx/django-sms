5f548d60f8fe75528d326d42573b1a98f9ba77c2
khuzaimx <huzaimthegenius1234@gmail.com>
2026-06-18 13:38:53 +0500

    statics

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Category, Product, Order, Customer
+from .models import category, Product, Order, Customer
 
-admin.site.register(Category)
+admin.site.register(category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Customer)
