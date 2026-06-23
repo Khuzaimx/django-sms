0e0247b994324b80bc14ea56fe75207533fde959
khuzaimx <huzaimthegenius1234@gmail.com>
2026-06-23 20:33:07 +0500

    sale models

--- store/models.py
+++ store/models.py
@@ -24,7 +24,8 @@ class Product(models.Model):
     category=models.ForeignKey(category, on_delete=models.CASCADE, default=1)
     description=models.TextField(null=True, blank=True)
     image=models.ImageField(upload_to='uploads/products/')
-
+    is_sale=models.BooleanField(default=False)
+    sale_price=models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     def __str__(self):
         return self.name
 
